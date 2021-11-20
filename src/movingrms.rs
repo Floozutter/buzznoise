4bba22fe267af2a5619adcefdf3c89bc9ec33edf
Floozutter <me@floozutter.site>
2021-11-19 19:34:43 -0800

    add extend method to trait MovingRMS

--- src/movingrms.rs
+++ src/movingrms.rs
@@ -1,4 +1,10 @@
 pub trait MovingRMS<T> {
     fn push(&mut self, value: T);
     fn rms(&self) -> Option<T>;
+
+    fn extend<U: IntoIterator<Item=T>>(&mut self, iter: U) {
+        for x in iter {
+            self.push(x);
+        }
+    }
 }
